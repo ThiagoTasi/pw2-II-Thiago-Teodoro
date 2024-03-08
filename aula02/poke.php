7dc301bde689bf017b1caec807e37622833cd79e
ThiagoTasi <161776684+ThiagoTasi@users.noreply.github.com>
2024-03-08 02:50:48 -0300

    Update poke.php

--- aula02/poke.php
+++ aula02/poke.php
@@ -1,5 +1,5 @@
   <?php
-    $pokemons = array("pikachu", "Charizard", "bubassauro", "Squirtle", ,"Eeavee" );
+    $pokemons = array("pikachu","Charizard","bubassauro","Squirtle","Eeavee");
 
     foreach($pokemons as $pokemon){
         if ($pokemon !== "Charmander"){
