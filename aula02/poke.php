5e63096ff1b05e953de9eb7f4f73e3ab170e4f68
ThiagoTasi <161776684+ThiagoTasi@users.noreply.github.com>
2024-03-08 02:48:05 -0300

    Update poke.php

--- aula02/poke.php
+++ aula02/poke.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    $pokemons = array("pikachu","Charizard";"bubassauro","Squirtle","Eeavee");
+  <?php
+    $pokemons = array("pikachu", "Charizard", "bubassauro", "Squirtle", ,"Eeavee" );
+
     foreach($pokemons as $pokemon){
         if ($pokemon !== "Charmander"){
         echo $pokemon. "<br>";
@@ -17,5 +9,4 @@
             
     ?>
     
-</body>
-</html>
+
