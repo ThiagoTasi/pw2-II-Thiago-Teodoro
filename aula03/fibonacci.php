6b6c931458d8444f0a55aeff6a3e31efa4ff185a
ThiagoTasi <161776684+ThiagoTasi@users.noreply.github.com>
2024-03-16 14:42:49 -0300

    Update fibonacci.php

--- aula03/fibonacci.php
+++ aula03/fibonacci.php
@@ -1,7 +1,15 @@
 <?php
 
-for ($x=0,$x=1;$x=1$x=2,$x=3;$x=5;$x=8;$x=13;$x=21;$x=34){
-    echo"Numero: $x <br>";
+$fibonacci = array("0", "1", '1", "2", "3", "5", "8", "13", "21", "34");
+
+for ($x=0; $x < count($fibonacci); $x++){
+echo 4fibinacci[$x], "<br>"
+}
+$x = 0;
+
+while($x < count($fibonacci)){
+echo $fibonacci[$x], "<br>";
+$x++;
 }
 
 
