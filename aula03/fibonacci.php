97d2f45da0ebbf4eeb196676f70aa9443eb49196
Thiago Teodoro <thiagosilvateo@gmail.com>
2024-03-18 19:20:33 -0300

    Atividades fibonacci, par impar e maior e menor.

--- aula03/fibonacci.php
+++ aula03/fibonacci.php
@@ -1,17 +1,14 @@
 <?php
 
-$fibonacci = array("0", "1", '1", "2", "3", "5", "8", "13", "21", "34");
+$fibonacci = array("0", "1", "1", "2", "3", "5", "8", "13", "21", "34");
 
 for ($x=0; $x < count($fibonacci); $x++){
-echo $fibonacci[$x], "<br>"
+echo $fibonacci[$x], "<br>";
 }
 $x = 0;
 
-while($x < count($fibonacci)){
+while($x <count($fibonacci)){
 echo $fibonacci[$x], "<br>";
 $x++;
 }
-
-
-
 ?>
