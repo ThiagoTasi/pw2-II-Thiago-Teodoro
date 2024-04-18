24e76555707f6e6b6aab72b1ceae88b68eb61e38
Thiago Tasi <thiagosilvateo@gmail.com>
2024-04-18 19:31:10 -0300

    termino do exercicio

---
+++ aula05/form3.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<body>
+    <form action="ex3.php" method="POST">
+        <label for="nota1">1º Nota:</label> <input type="number" name="nota1"><br>
+        <label for="nota2">2º Nota:</label> <input type="number" step=".13" name="nota2"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
