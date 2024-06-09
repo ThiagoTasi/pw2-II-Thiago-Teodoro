fb8ca0c5b34420878559ab882f18dfc15d6c2fbb
Thiago Tasi <thiagosilvateo@gmail.com>
2024-06-09 02:33:24 -0300

    corrigindo

--- Projeto 2/index.php
+++ Projeto 2/index.php
@@ -63,73 +63,72 @@ try {
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link
-             href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-             rel="stylesheet"
-             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-             crossorigin="anonymous">
-        <title>Agenda</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Agenda</title>
 </head>
-<body>
-    <div class="p-3 mb-2 bg-secondary text-white container text-center rounded">
-    <div class="container mx-auto py-8">
+<body class="bg-gray-100">
+    <div class="p-3 mb-2 bg-gray-700 text-white container text-center rounded">
         <h2 class="text-2xl font-bold mb-4">Insira seus dados:</h2>
-        <form method="POST">
-            <div>
-                <label for="nome">Nome:</label>
-                <input type="text" id="nome" name="nome" required="required" class="form-input mt-1 block w-full">
+    </div>
+    <div class="container mx-auto py-8">
+        <form method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+                <label for="nome" class="block text-gray-700 text-sm font-bold mb-2">Nome:</label>
+                <input type="text" id="nome" name="nome" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="endereco">Endereco:</label>
-                <input type="text" id="endereco" name="endereco" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="endereco" class="block text-gray-700 text-sm font-bold mb-2">Endereço:</label>
+                <input type="text" id="endereco" name="endereco" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="cidade">Cidade:</label>
-                <input type="text" id="cidade" name="cidade" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="cidade" class="block text-gray-700 text-sm font-bold mb-2">Cidade:</label>
+                <input type="text" id="cidade" name="cidade" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="bairro">Bairro:</label>
-                <input type="text" id="bairro" name="bairro" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="bairro" class="block text-gray-700 text-sm font-bold mb-2">Bairro:</label>
+                <input type="text" id="bairro" name="bairro" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="estado">Estado:</label>
-                <input type="text" id="estado" name="estado" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="estado" class="block text-gray-700 text-sm font-bold mb-2">Estado:</label>
+                <input type="text" id="estado" name="estado" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email:</label>
+                <input type="email" id="email" name="email" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="telefone">Telefone:</label>
-                <input type="tel" id="telefone" name="telefone" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="telefone" class="block text-gray-700 text-sm font-bold mb-2">Telefone:</label>
+                <input type="tel" id="telefone" name="telefone" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="celular">Celular:</label>
-                <input type="tel" id="celular" name="celular" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="celular" class="block text-gray-700 text-sm font-bold mb-2">Celular:</label>
+                <input type="tel" id="celular" name="celular" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="cpf">CPF:</label>
-                <input type="text" id="cpf" name="cpf" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="cpf" class="block text-gray-700 text-sm font-bold mb-2">CPF:</label>
+                <input type="text" id="cpf" name="cpf" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="genero">Genero:</label>
-                <input type="text" id="genero" name="genero" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="genero" class="block text-gray-700 text-sm font-bold mb-2">Genero:</label>
+                <input type="text" id="genero" name="genero" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="data_nascimento">Data_Nascimento:</label>
-                <input type="date" id="data_nascimento" name="data_nascimento" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="data_nascimento" class="block text-gray-700 text-sm font-bold mb-2">Data de Nascimento:</label>
+                <input type="date" id="data_nascimento" name="data_nascimento" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="profissao">Profissao:</label>
-                <input type="text" id="profissao" name="profissao" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="profissao" class="block text-gray-700 text-sm font-bold mb-2">Profissão:</label>
+                <input type="text" id="profissao" name="profissao" required class="form-input mt-1 block w-full">
             </div>
-            <div>
-                <label for="nacionalidade">Nacionalidade:</label>
-                <input type="text" id="nacionalidade" name="nacionalidade" required="required" class="form-input mt-1 block w-full">
+            <div class="mb-4">
+                <label for="nacionalidade" class="block text-gray-700 text-sm font-bold mb-2">Nacionalidade:</label>
+                <input type="text" id="nacionalidade" name="nacionalidade" required class="form-input mt-1 block w-full">
             </div>
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Enviar</button>
+            <div class="flex items-center justify-between">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Enviar</button>
+            </div>
         </form>
     </div>
 </body>
--- Projeto/bancodedados.php
+++ Projeto/bancodedados.php
@@ -63,7 +63,7 @@ try {
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <title>Formulário PHP PDO com Tailwind CSS</title>
+    <title>Agenda</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="block text-green-700">
@@ -72,70 +72,58 @@ try {
         <form method="POST" class="max-w-lg">
             <div class="mb-4">
                 <label for="nome" class="block text-green-700">Nome:</label>
-                <input type="text" id="nome" name="nome" required="required" class="form-input mt-1 block w-full">
+                <input type="text" id="nome" name="nome" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="endereco" class="block text-gray-700">Endereço:</label>
-                <input type="text" id="endereco" name="endereco" required="required" class="form-input mt-1 block w-full">
+                <label for="endereco" class="block text-green-700">Endereço:</label>
+                <input type="text" id="endereco" name="endereco" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="cidade" class="block text-gray-700">Cidade:</label>
-                <input type="text" id="cidade" name="cidade" required="required" class="form-input mt-1 block w-full">
+                <label for="cidade" class="block text-green-700">Cidade:</label>
+                <input type="text" id="cidade" name="cidade" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="bairro" class="block text-gray-700">Bairro:</label>
-                <input type="text" id="bairro" name="bairro" required="required" class="form-input mt-1 block w-full">
+                <label for="bairro" class="block text-green-700">Bairro:</label>
+                <input type="text" id="bairro" name="bairro" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="estado" class="block text-gray-700">Estado:</label>
-                <input type="text" id="estado" name="estado" required="required" class="form-input mt-1 block w-full">
+                <label for="estado" class="block text-green-700">Estado:</label>
+                <input type="text" id="estado" name="estado" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="email" class="block text-gray-700">Email:</label>
-                <input type="email" id="email" name="email" required="required" class="form-input mt-1 block w-full">
+                <label for="email" class="block text-green-700">Email:</label>
+                <input type="email" id="email" name="email" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="telefone" class="block text-gray-700">Telefone:</label>
-                <input type="tel" id="telefone" name="telefone" required="required" class="form-input mt-1 block w-full">
+                <label for="telefone" class="block text-green-700">Telefone:</label>
+                <input type="tel" id="telefone" name="telefone" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="celular" class="block text-gray-700">Celular:</label>
-                <input type="tel" id="celular" name="celular" required="required" class="form-input mt-1 block w-full">
+                <label for="celular" class="block text-green-700">Celular:</label>
+                <input type="tel" id="celular" name="celular" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="cpf" class="block text-gray-700">CPF:</label>
-                <input type="text" id="cpf" name="cpf" required="required" class="form-input mt-1 block w-full">
+                <label for="cpf" class="block text-green-700">CPF:</label>
+                <input type="text" id="cpf" name="cpf" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="genero" class="block text-gray-700">Gênero:</label>
-                <input type="text" id="genero" name="genero" required="required" class="form-input mt-1 block w-full">
+                <label for="genero" class="block text-green-700">Genero:</label>
+                <input type="text" id="genero" name="genero" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="data_nascimento" class="block text-gray-700">Data_Nascimento:</label>
-                <input type="date" id="data_nascimento" name="data_nascimento" required="required" class="form-input mt-1 block w-full">
+                <label for="data_nascimento" class="block text-green-700">Data de Nascimento:</label>
+                <input type="date" id="data_nascimento" name="data_nascimento" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="profissao" class="block text-gray-700">Profissão:</label>
-                <input type="text" id="profissao" name="profissao" required="required" class="form-input mt-1 block w-full">
+                <label for="profissao" class="block text-green-700">Profissão:</label>
+                <input type="text" id="profissao" name="profissao" required class="form-input mt-1 block w-full">
             </div>
             <div class="mb-4">
-                <label for="nacionalidade" class="block text-gray-700">Nacionalidade:</label>
-                <input type="text" id="nacionalidade" name="nacionalidade" required="required" class="form-input mt-1 block w-full">
+                <label for="nacionalidade" class="block text-green-700">Nacionalidade:</label>
+                <input type="text" id="nacionalidade" name="nacionalidade" required class="form-input mt-1 block w-full">
             </div>
             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Enviar</button>
         </form>
     </div>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-                                                       
-
-                                       
