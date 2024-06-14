23abd9909d8f33077492720a9957904bb1bb9375
Thiago Tasi <thiagosilvateo@gmail.com>
2024-06-13 22:17:48 -0300

    alteracoes

--- Projeto 2/index.php
+++ Projeto 2/index.php
@@ -76,7 +76,7 @@ try {
         <form method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <div class="mb-4">
                 <label for="nome" class="block text-gray-700 text-sm font-bold mb-2">Nome:</label>
-                <input type="text" id="nome" name="nome" required class="form-input mt-1 block w-full">
+                <input type="text" id="nome" name="nome" required class="border-2 border-gray-900" size="30">
             </div>
             <div class="mb-4">
                 <label for="endereco" class="block text-gray-700 text-sm font-bold mb-2">Endereço:</label>
